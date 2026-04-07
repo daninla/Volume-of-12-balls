a72b490884c2495c2a23041c806b5cb79980951f
danila <idanila331@gmail.com>
2026-04-07 18:58:01 +0300

    remove duble wall

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 "use strict";
 
-const DIAMETER_OF_SMALLEST_BALL = 0.5;
-const WALL_OF_BALLS = 0.01;
-let radius = DIAMETER_OF_SMALLEST_BALL / 2 - WALL_OF_BALLS;
-let volume = 0;
+const SMALLEST_OUTER_DIAMETER = 0.5;
+const WALL_THICKNESS = 0.01;       
+
+let totalVolume = 0;
+let radius = (SMALLEST_OUTER_DIAMETER / 2) - WALL_THICKNESS;
 
 for (let i = 0; i < 12; i++) {
-  volume += (4 / 3) * Math.PI * radius ** 3;
-  radius += WALL_OF_BALLS * 2;
+  totalVolume += (4 / 3) * Math.PI * radius ** 3;
+  radius += WALL_THICKNESS;
 }
-console.log(`Об'єм: ${volume.toFixed(2)} метрів кубічних`);
+
+console.log(`Загальний внутрішній об'єм: ${totalVolume.toFixed(2)} метрів кубічних`);
